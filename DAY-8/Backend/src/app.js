f06162b26b6900308d33520a3d360e46e48a501a
Vishxlll20 <vishalk785032@gmail.com>
2026-02-10 14:03:53 +0530

    Frontend backend sameURL

--- DAY-8/Backend/src/app.js
+++ DAY-8/Backend/src/app.js
@@ -1,54 +1,60 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
-const notesModel = require("./models/notes.model")
-const cors = require("cors")
+const app = express();
+const notesModel = require("./models/notes.model");
+const cors = require("cors");
+const path = require("path");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
 
+app.post("/api/notes", async (req, res) => {
+  const { title, description } = req.body;
 
-app.post('/api/notes',async(req,res)=>{
-    const {title, description} = req.body
+  const note = await notesModel.create({
+    title,
+    description,
+  });
 
-    const note = await notesModel.create({
-        title, description
-    })
+  res.status(201).json({
+    message: "Note Created Successfully",
+    note,
+  });
+});
 
-    res.status(201).json({
-        message : "Note Created Successfully",
-        note
-    })
-})
+app.get("/api/notes", async (req, res) => {
+  const notes = await notesModel.find();
 
-app.get("/api/notes", async (req,res) => {
-    const notes = await notesModel.find()
+  res.status(200).json({
+    message: "Data Fetched Successfully",
+    notes,
+  });
+});
 
-    res.status(200).json({
-        message : 'Data Fetched Successfully',
-        notes
-    })
-})
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
 
-app.delete("/api/notes/:id",async (req,res)=>{
-    const id = req.params.id
+  await notesModel.findByIdAndDelete(id);
 
-   await notesModel.findByIdAndDelete(id)
+  res.status(200).json({
+    message: "Note deleted Successfully",
+  });
+});
 
-    res.status(200).json({
-        message : "Note deleted Successfully"
-    })
-})
+app.patch("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const { description } = req.body;
 
-app.patch("/api/notes/:id",async (req,res)=>{
-    const id = req.params.id;
-    const {description} = req.body
+  await notesModel.findByIdAndUpdate(id, { description });
 
-    await notesModel.findByIdAndUpdate(id, {description})
+  res.status(200).json({
+    message: "Note Updated Successfully",
+  });
+});
 
-    res.status(200).json({
-        message : "Note Updated Successfully"
-    })
-})
+app.use("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+});
 
-module.exports = app
+module.exports = app;
--- DAY-8/Frontend/src/App.jsx
+++ DAY-8/Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useState } from "react";
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
   const [notes, setnotes] = useState([
@@ -22,23 +22,77 @@ const App = () => {
     },
   ]);
 
-  axios.get('http://localhost:3000/api/notes')
-  .then((res)=>{
-    setnotes(res.data.notes)
-  })
+  function getdata() {
+    axios.get("http://localhost:3000/api/notes").then((res) => {
+      setnotes(res.data.notes);
+    });
+  }
 
+  useEffect(() => {
+    getdata();
+  }, []);
+
+  function submithandler(e) {
+    e.preventDefault()
+    const { title, description } = e.target.elements
+    console.log(title.value, description.value)
+    axios.post("http://localhost:3000/api/notes", {
+        title: title.value,
+        description: description.value,
+      })
+      .then(res => {
+        console.log(res.data);
+
+        getdata()
+      });
+  }
+
+  function deletehandler(noteId){
+    console.log(noteId)
+    axios.delete("http://localhost:3000/api/notes/"+noteId)
+    .then(res=>{
+      console.log(res.data);
+      getdata();
+    })
+
+    }
+  
   return (
     <div className="bg-black h-screen p-10">
-      {notes.map((notes,idx) => {
+      <form className="ml-50" onSubmit={submithandler}>
+        <input
+        name="title"
+          placeholder="Enter Title"
+          type="text"
+          className="bg-gray-300 rounded-xl text-black p-2 m-2"
+        />
+        <input
+        name="description"
+          placeholder="Enter Description"
+          type="text"
+          className="bg-gray-300 rounded-xl text-black p-2 m-2 "
+        />
+        <button className="bg-gray-300 rounded-xl text-black p-2">
+          Submit form
+        </button>
+      </form>
+      <div className="flex flex-wrap">
+         {notes.map((notes, idx) => {
         return (
-          <div key = {idx}className="bg-gray-500 w-80 text-2xl p-5 rounded-2xl m-5 ml-50">
+          <div
+            key={idx}
+            className="bg-gray-500 w-80 text-2xl p-5 rounded-2xl m-5 ml-50"
+          >
             <h1 className="text-white">{notes.title}</h1>
             <p className="text-white">{notes.description}</p>
+            <button className="bg-gray-300 p-2 rounded-xl" onClick={()=>{deletehandler(notes._id)}}>Delete</button>
           </div>
         );
       })}
+      </div>
+     
     </div>
   );
-};
+}
 
 export default App;
